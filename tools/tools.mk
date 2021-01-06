452222c136550c3f358e0bc6e6c54b0ec52fc85a
Stijn Martens <stijnmartens@3dbyflow.com>
2021-01-06 18:33:16 +0100

    Windows debug working

--- tools/tools.mk
+++ tools/tools.mk
@@ -5,11 +5,11 @@ prog:
 
 # Run openocd to allow GDB connections at :3333
 oocd:
-	openocd-esp32.exe -f ./tools/ftdi_mini_module.cfg -f target/esp32.cfg -c "bindto 0.0.0.0" -c "init"
+	openocd-esp32.exe -f ./tools/ftdi_mini_module.cfg -f target/esp32.cfg -c "init"
 
 # Run GDB to connect to OpenOCD
-gdb:
-	xtensa-esp32-elf-gdb build/$(APP).elf -nh -ex "target remote :3333"
+gdboocd:
+	xtensa-esp32-elf-gdb.exe build/$(APP).elf -nh -ex "target remote | openocd-esp32.exe -f /home/stijn/dev/better-esp-idf/tools/ftdi_mini_module.cfg -f target/esp32.cfg -c 'gdb_port pipe; log_output openocd.log' -c 'init'openocd-esp32.exe -f ./tools/ftdi_mini_module.cfg -f target/esp32.cfg -c 'gdb_port pipe; log_output openocd.log' -c 'init'"
 
 # Generate C/C++ include paths for all components and add them to settings.json
 intellisense:
